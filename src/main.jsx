532dbb4bcd80a4aee0c16b6772ef682893d9fde5
Igor <iluzarraga@gmail.com>
2023-02-03 10:41:31 +0100

    fix problems with Mock Service Worker

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
+import { worker } from './mocks/browser'
 
-// if (process.env.NODE_ENV === 'development') {
-//   const { worker } = require('./mocks/browser')
-//   worker.start()
-// }
+if (process.env.NODE_ENV === 'development') {
+  //  const { worker } = require('./mocks/browser')
+  //import { worker } from ('./mocks/browser')
+  worker.start()
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
